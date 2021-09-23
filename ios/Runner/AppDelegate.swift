618df3ecc1a80ac1181d9368151562e89c193cd7
“David <davidestebandeveloper@gmail.com>
2021-09-22 23:30:32 -0300

    fix bug csm version beta 1

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,14 +1,18 @@
 import UIKit
+import Firebase
 import Flutter
 import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
+
+
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-     GMSServices.provideAPIKey("AIzaSyD_lFpA7YI75XFW12HdnpS32Y8q3k-v31Q")
+    FirebaseApp.configure()
+    GMSServices.provideAPIKey("AIzaSyD_lFpA7YI75XFW12HdnpS32Y8q3k-v31Q")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
